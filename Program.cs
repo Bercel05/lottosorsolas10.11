f748291af9f0050656dc6a02a679ea7d1afff9c3
MarkusBercel <markusb@kkszki.hu>
2024-10-11 12:39:57 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lottosorsolas_10._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        {
+            {
+                Random random = new Random();
+                HashSet<int> lottoNumbers = new HashSet<int>(); 
+
+                while (lottoNumbers.Count < 6)
+                {
+                    lottoNumbers.Add(random.Next(1, 50)); 
+                }
+
+               
+                Console.WriteLine("A generált lottó számok: " + string.Join(", ", lottoNumbers));
+            }
+        }
+
+    }
+}
+}
